db51ab760fb8318025de5181052bd121b630be03
LouiseAllen192 <louise.allen192@gmail.com>
2017-03-15 14:30:23 +0000

    Refactoring of Display class

--- GraphicsManager/Display.java
+++ GraphicsManager/Display.java
@@ -7,12 +7,12 @@ public class Display implements IDisplay {
     private String title;
     private JFrame frame;
     private Canvas canvas;
-    private int screenWidth, screenHeight;
+    private Dimension screen;
 
     public Display(String title, double screenWidth, double screenHeight)  {
         this.title = title;
-        this.screenWidth = (int) screenWidth;
-        this.screenHeight = (int) screenHeight;
+        this.screen = new Dimension();
+        screen.setSize(screenWidth, screenHeight);
     }
 
     public Canvas getCanvas() {
@@ -21,15 +21,15 @@ public class Display implements IDisplay {
 
     public void createDisplay() {
         frame = new JFrame(title);
-        frame.setSize(this.screenWidth , this.screenHeight);
+        frame.setSize(this.screen);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(screenWidth, screenHeight));
+        frame.setMinimumSize(this.screen);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
 
         canvas = new Canvas();
-        canvas.setPreferredSize(new Dimension(this.screenWidth, this.screenHeight));
+        canvas.setPreferredSize(this.screen);
 
         frame.add(canvas);
         frame.pack();
